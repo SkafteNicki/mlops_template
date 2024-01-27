6a0f6faf107aa60ad4a7f68f76472c4b5a838e31
Lukas Leindals <s183920@student.dtu.dk>
2024-01-27 22:19:50 +0100

    add dummy model

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/models/model.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/models/model.py
@@ -1,26 +1,20 @@
-import torch
 
-class MyNeuralNet(torch.nn.Module):
-    """ Basic neural network class. 
-    
-    Args:
-        in_features: number of input features
-        out_features: number of output features
-    
+class PlaceholderModel(object):
     """
-    def __init__(self, in_features: int, out_features: int) -> None:
-        self.l1 = torch.nn.Linear(in_features, 500)
-        self.l2 = torch.nn.Linear(500, out_features)
-        self.r = torch.nn.ReLU()
-    
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the model.
-        
-        Args:
-            x: input tensor expected to be of shape [N,in_features]
-
-        Returns:
-            Output tensor with shape [N,out_features]
-
+    Description of the model
+    """
+    def __init__(self):
         """
-        return self.l2(self.r(self.l1(x)))
+        Description of the model initialization
+        
+        Parameters
+        ----------
+        param1 : type
+            Description of param1
+            
+        Returns
+        -------
+        type
+            Description of returned object
+        """
+        raise NotImplementedError
