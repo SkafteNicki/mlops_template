0a134103d2871217941f847f94d900d61acf8d01
s183920 <s183920@student.dtu.dk>
2024-01-31 15:05:03 +0100

    add docstring

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/tools/seeding.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/tools/seeding.py
@@ -1,11 +1,19 @@
 import numpy as np
 import random
 
-def set_seed(seed):
+def set_seed(seed: int) -> None:
+    """
+    Sets the random seed
+    
+    Parameters
+    ----------
+    seed: int
+        The seed to set
+    """
     np.random.seed(seed)
     random.seed(seed)
     
     # try:
-    torch.manual_seed(seed)
+    # torch.manual_seed(seed)
     # except
     
