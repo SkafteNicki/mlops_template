65468d217357722c63c6b26954f1260f5b7dd600
s183920 <s183920@student.dtu.dk>
2024-01-31 14:53:55 +0100

    setup config

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/training/trainer.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/training/trainer.py
@@ -1,5 +1,8 @@
+import hydra
 
 
 class Trainer:
-    def __init__(self):
+    @hydra.main(config_path = "../configs", config_name = "config")
+    def __init__(self, cfg):
+        self.config = cfg.train_config
         raise NotImplementedError
