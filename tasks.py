45d28e6cc416df3525e146d88d01521f8a853f26
Nicki Skafte <skaftenicki@gmail.com>
2024-12-16 11:00:51 +0100

    small update

--- tasks.py
+++ tasks.py
@@ -1,5 +1,12 @@
 from invoke import task
 
+
 @task
 def template(ctx):
     ctx.run("cookiecutter -f --no-input .")
+
+
+@task
+def requirements(ctx):
+    ctx.run("python -m pip install --upgrade pip")
+    ctx.run("pip install -r requirements.txt")
