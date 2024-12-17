bbdfed7628f9ec7e9fdae27d47a794d4e6ecc1f6
Nicki Skafte <skaftenicki@gmail.com>
2024-12-17 16:08:50 +0100

    fix spelling mistakes

--- tasks.py
+++ tasks.py
@@ -3,10 +3,12 @@ from invoke import task
 
 @task
 def template(ctx):
-    ctx.run("cookiecutter -f --no-input .")
+    """Create a new project from the template."""
+    ctx.run("cookiecutter -f --no-input --verbose .")
 
 
 @task
 def requirements(ctx):
+    """Install project requirements."""
     ctx.run("python -m pip install --upgrade pip")
     ctx.run("pip install -r requirements.txt")
--- {{ cookiecutter.repo_name }}/tasks.py
+++ {{ cookiecutter.repo_name }}/tasks.py
@@ -7,7 +7,8 @@ PYTHON_VERSION = "{{ cookiecutter.python_version }}"
 
 # Setup commands
 @task
-def create_enviroment(ctx):
+def create_environment(ctx):
+    """Create a new conda environment for project."""
     ctx.run(
         f"conda create --name {PROJECT_NAME} python={PYTHON_VERSION} pip --no-default-packages --yes",
         echo=True,
@@ -16,6 +17,7 @@ def create_enviroment(ctx):
 
 @task
 def requirements(ctx):
+    """Install project requirements."""
     ctx.run("pip install -U pip setuptools wheel", echo=True, pty=not WINDOWS)
     ctx.run("pip install -r requirements.txt", echo=True, pty=not WINDOWS)
     ctx.run("pip install -e .", echo=True, pty=not WINDOWS)
@@ -23,39 +25,40 @@ def requirements(ctx):
 
 @task(requirements)
 def dev_requirements(ctx):
+    """Install development requirements."""
     ctx.run("pip install -e .[\"dev\"]", echo=True, pty=not WINDOWS)
-
-
-@task
-def clean(ctx):
-    ctx.run("find . -type f -name \"*.py[co]\" -delete", echo=True, pty=not WINDOWS)
-    ctx.run("find . -type d -name \"__pycache__\" -delete")
 
 # Project commands
 @task
 def preprocess_data(ctx):
+    """Preprocess data."""
     ctx.run(f"python src/{PROJECT_NAME}/data.py data/raw data/processed", echo=True, pty=not WINDOWS)
 
 @task
 def train(ctx):
+    """Train model."""
     ctx.run(f"python src/{PROJECT_NAME}/train.py", echo=True, pty=not WINDOWS)
 
 @task
 def test(ctx):
+    """Run tests."""
     ctx.run("coverage run -m pytest tests/", echo=True, pty=not WINDOWS)
     ctx.run("coverage report -m", echo=True, pty=not WINDOWS)
 
 @task
 def docker_build(ctx):
+    """Build docker images."""
     ctx.run("docker build -t train:latest dockerfiles/ -f dockerfiles/train.Dockerfile", echo=True, pty=not WINDOWS)
     ctx.run("docker build -t api:latest dockerfiles/ -f dockerfiles/api.Dockerfile", echo=True, pty=not WINDOWS)
 
 # Documentation commands
 @task(dev_requirements)
 def build_docs(ctx):
+    """Build documentation."""
     ctx.run("mkdocs build --config-file docs/mkdocs.yaml --site-dir build", echo=True, pty=not WINDOWS)
 
 
 @task(dev_requirements)
 def serve_docs(ctx):
+    """Serve documentation."""
     ctx.run("mkdocs serve --config-file docs/mkdocs.yaml", echo=True, pty=not WINDOWS)
