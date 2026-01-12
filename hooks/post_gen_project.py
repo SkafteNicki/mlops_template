b9468e9565a46329cae516d70ab27d56ec2310a5
Nicki Skafte <skaftenicki@gmail.com>
2026-01-12 07:16:39 +0100

    update workflows

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -67,6 +67,8 @@ if deps_manager == "uv":
     Path("pyproject_uv.toml").rename("pyproject.toml")
     Path("tasks_pip.py").unlink()
     Path("tasks_uv.py").rename("tasks.py")
+    Path("README_pip.md").unlink()
+    Path("README_uv.md").rename("README.md")
     if project_structure == "advance":
         Path("dockerfiles/api_pip.dockerfile").unlink()
         Path("dockerfiles/api_uv.dockerfile").rename("dockerfiles/api.dockerfile")
@@ -74,6 +76,12 @@ if deps_manager == "uv":
         Path("dockerfiles/train_uv.dockerfile").rename("dockerfiles/train.dockerfile")
         Path(".devcontainer/post_create_pip.sh").unlink()
         Path(".devcontainer/post_create_uv.sh").rename(".devcontainer/post_create.sh")
+        Path(".github/workflows/tests_pip.yaml").unlink()
+        Path(".github/workflows/tests_uv.yaml").rename(".github/workflows/tests.yaml")
+        Path(".github/workflows/linting_pip.yaml").unlink()
+        Path(".github/workflows/linting_uv.yaml").rename(".github/workflows/linting.yaml")
+        Path(".github/dependabot_pip.yaml").unlink()
+        Path(".github/dependabot_uv.yaml").rename(".github/dependabot.yaml")
 
 if deps_manager == "pip":
     logger.info("Renaming files and folders for the pip template.")
@@ -81,6 +89,8 @@ if deps_manager == "pip":
     Path("pyproject_pip.toml").rename("pyproject.toml")
     Path("tasks_uv.py").unlink()
     Path("tasks_pip.py").rename("tasks.py")
+    Path("README_uv.md").unlink()
+    Path("README_pip.md").rename("README.md")
     if project_structure == "advance":
         Path("dockerfiles/api_uv.dockerfile").unlink()
         Path("dockerfiles/api_pip.dockerfile").rename("dockerfiles/api.dockerfile")
@@ -88,6 +98,12 @@ if deps_manager == "pip":
         Path("dockerfiles/train_pip.dockerfile").rename("dockerfiles/train.dockerfile")
         Path(".devcontainer/post_create_uv.sh").unlink()
         Path(".devcontainer/post_create_pip.sh").rename(".devcontainer/post_create.sh")
+        Path(".github/workflows/tests_uv.yaml").unlink()
+        Path(".github/workflows/tests_pip.yaml").rename(".github/workflows/tests.yaml")
+        Path(".github/workflows/linting_uv.yaml").unlink()
+        Path(".github/workflows/linting_pip.yaml").rename(".github/workflows/linting.yaml")
+        Path(".github/dependabot_uv.yaml").unlink()
+        Path(".github/dependabot_pip.yaml").rename(".github/dependabot.yaml")
 
 if use_coding_agent_support:
     logger.info("Adding coding agent support files.")
